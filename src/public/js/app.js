b5fd8d7f6c0578041cce7ae474c8fe0a91a23fda
Dongmin <emthrology@gmail.com>
2021-09-20 23:04:26 +0900

    socketIO/5.edit nickname before enter room

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,42 +1,6 @@
-// const messageList = document.querySelector("ul");
-// const messageForm = document.querySelector("#message");
-// const nickForm = document.querySelector("#nickName")
-// const socket = new WebSocket(`ws://${window.location.host}`);
-
-// socket.addEventListener("open",() => {
-//   console.log("connected to server");
-// });
-
-// socket.addEventListener("message", (msg) => {
-//   // console.log(`New message: "${msg.data}"`);
-//   const li = document.createElement("li");
-//   li.innerText = msg.data;
-//   messageList.append(li);
-// });
-
-// socket.addEventListener("close", () => {
-//   console.log("disconnected from server");
-// });
-
-// function makeMessage(type, payload) {
-//   const msg = { type, payload };
-//   return JSON.stringify(msg);
-// } 
-
-// function handleSubmit(evt, formType) {
-//   evt.preventDefault();
-//   const input = formType.querySelector("input");
-//   socket.send(makeMessage(formType.id, input.value));
-//   input.value = "";
-// }
-
-// messageForm.addEventListener("submit", (e) => handleSubmit(e, messageForm));
-// nickForm.addEventListener("submit", (e) => handleSubmit(e, nickForm));
-
-
 const socket = io();
 const welcome = document.querySelector("#welcome");
-const form = welcome.querySelector("form");
+const infoForm = welcome.querySelector("form");
 const room = document.querySelector("#room");
 
 room.hidden = true;
@@ -45,18 +9,35 @@ let roomName;
 
 function handleRoomSubmit(evt,target) {
   evt.preventDefault();
-  const input = target.querySelector("input");
+  const room = target.querySelector("#room-info");
+  const nick = target.querySelector("#nick");
   //socketIO is able to make custom event, which is why socketIO is event-based
   //in SocketIO, developer dosen't have to make js object into JSON 
   //third parameter is callback from server
-  socket.emit("enter_room", 
-    { payload: input.value },
+  socket.emit("enter_room", { room:room.value, nick:nick.value },
     (...args) => {
-      console.log("bacnend job is done");
       console.log(args); 
       showRoom();
+      addMessage(`You entered the room:${room.value}`);
   });
-  roomName = input.value;
+  roomName = room.value;
+}
+
+function haneldNicknameSubmit(evt,roomName,target) {
+  evt.preventDefault();
+  const input = target.querySelector("#name input");
+  socket.emit("nickname", input.value,roomName, () => {
+    addMessage(`You have changed your nickname into ${input.value}`);
+  })
+}
+
+function handleMessageSubmit(evt,roomName,target) {
+  evt.preventDefault();
+  const input = target.querySelector("#message input");
+  const value = input.value;
+  socket.emit("message", value,roomName, () => {
+    addMessage(`You: ${value}`);
+  });
   input.value = "";
 }
 
@@ -65,6 +46,10 @@ function showRoom() {
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
+  const msgForm = room.querySelector("#message");
+  const nameForm = room.querySelector("#name");
+  msgForm.addEventListener("submit",e => handleMessageSubmit(e,roomName,room));
+  nameForm.addEventListener("submit",e => haneldNicknameSubmit(e,roomName,room));
 }
 
 function addMessage(msg) {
@@ -74,10 +59,15 @@ function addMessage(msg) {
   ul.appendChild(li);
 }
 
-form.addEventListener("submit", (e) => handleRoomSubmit(e, form));
+infoForm.addEventListener("submit", (e) => handleRoomSubmit(e, infoForm));
 
 //handle custom event from server
-socket.on("welcome", () => {
-  addMessage("Someone joined");
+socket.on("welcome", (user) => {
+  addMessage(`${user} entered`);
 });
 
+socket.on("message", addMessage);
+
+socket.on("bye", (user) => {
+  addMessage(`${user} left`);
+});
--- src/server.js
+++ src/server.js
@@ -21,50 +21,42 @@ const wsServer = SocketIO(httpServer);
 
 //SocketIO
 wsServer.on("connection", socket => {
+  
   //onAny: eventListener of any kind
   socket.onAny(event => {
     console.log(`Socket Event: ${event}`);
   });
   //socketIO is able to make custom event, which is why socketIO is event-based
   socket.on("enter_room", (roomObj, done) => {
-    const roomName = roomObj.payload;
+    const roomName = roomObj.room;
+    socket["nickname"] = roomObj.nick || "annonymous_default";
     socket.join(roomName);
     
     //when function done is called, function at the front(emit's thrid argument) is invoked
     //NOT the backend's function
     setTimeout(() => done("this is from backend"),0); //mock async
-    socket.to(roomName).emit("welcome"); //emit custom event to browser
+    socket.to(roomName).emit("welcome", socket.nickname); //emit custom event to browser
 
+  });
+
+  socket.on("nickname", (nickname,roomName,done) => {
+    const oldNick = socket["nickname"] || "annonymous_default";
+    socket["nickname"] = nickname;
+    socket.to(roomName).emit("message",`${oldNick} has changed his/her nickname to ${nickname}`);
+    done();
+  });
+
+  socket.on("message", (msg,roomName,done) => {
+    socket.to(roomName).emit("message",`${socket.nickname}: ${msg}`);
+    done();
+  })
+
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname));
   });
 })
 
-/* WebSocket 
-const wss = new WebSocket.Server({ server });
-const sockets = [];
-wss.on("connection", (socket) => {
-  sockets.push(socket); //??
-  
-  socket["nickName"] = "annonymous";
-  console.log("connected to browser");
-  socket.send("connected to the server");
-  socket.on("message",(msg) => {
-    const msgObj = JSON.parse(msg);
-    switch(msgObj.type) {
-      case "message":
-        sockets.forEach(aSocket => {
-          aSocket.send(`${socket.nickName}: ${msgObj.payload}`);
-        });
-        break;
-      case "nickName":
-        //put nickname in the socket object element of sockets array;
-        socket["nickName"] = msgObj.payload;
-    }
-  });
-  socket.on("close", () => {
-    console.log("disconnected from browser");
-  });
-});
-*/
+
 
 const handleListen = () => console.log("listening on 'http://localhost:3000'");
 
